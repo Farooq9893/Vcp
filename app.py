f3fe7867bf04553d116f3d7e9ee949828dfc14da
Farooq9893 <farooq9893@gmail.com>
2025-06-03 00:22:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import streamlit as st
-import yfinance as yf
-import pandas as pd
+import streamlit as st 
+import yfinance as yf 
+import pandas as pd 
 
 st.set_page_config(page_title="AI Stock Dashboard", layout="wide")
 
